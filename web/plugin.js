540bb7828b0e245e8782787ef77cf5effe64aa04
ctalau <cristitalau@gmail.com>
2016-07-01 13:25:08 +0300

    Set the URL only after the editor has loaded.

--- web/plugin.js
+++ web/plugin.js
@@ -166,15 +166,6 @@
   var WebdavFileBrowser = function() {
     var latestUrl = this.getLatestUrl();
     var latestRootUrl = this.getLatestRootUrl();
-    var currDocUrl = decodeURIComponent(sync.util.getURLParameter('url'));
-
-    // if the current root and url are not set we use
-    // the current document url.
-    if(!latestUrl && !latestRootUrl && currDocUrl) {
-      this.requestUrlInfo_(currDocUrl,
-        goog.bind(this.setUrlInfo, this));
-    }
-
     sync.api.FileBrowsingDialog.call(this, {
       initialUrl: latestUrl,
       root: latestRootUrl
@@ -486,6 +477,16 @@
 
   // register all the listeners on the file browser.
   registerFileBrowserListeners(fileBrowser);
+  goog.events.listen(workspace, sync.api.Workspace.EventType.EDITOR_LOADED, function(e) {
+    var currDocUrl = e.getUrl();
+    // if the current root and url are not set we use the current document url.
+
+    if (!fileBrowser.getLatestUrl() && !fileBrowser.getLatestRootUrl() && currDocUrl) {
+      fileBrowser.requestUrlInfo_(currDocUrl,
+        goog.bind(this.setUrlInfo, this));
+    }
+  });
+
   // the large icon url, hidpi enabled.
   var iconUrl = sync.util.computeHdpiIcon('../plugin-resources/webdav/Webdav70.png');
 
