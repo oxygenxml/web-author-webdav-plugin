e9527bc7043f3f3d9bca68de72b423ba3106de4d
mihai_coanda <coanda.mihai@gmail.com>
2018-01-09 12:18:52 +0200

    The autoSaveInterval set in the admin page is now overridden by the URL parameter.

--- web/autosave.js
+++ web/autosave.js
@@ -1,9 +1,13 @@
 (function() {
   /** BEFORE_EDITOR_LOADED */
   goog.events.listen(workspace, sync.api.Workspace.EventType.BEFORE_EDITOR_LOADED, function(e) {
-    var autoSaveInterval = parseInt(sync.options.PluginsOptions.getClientOption('webdav_autosave_interval'));
-    if (e.options.url.match(/^webdav-https?:/)) {
-      e.options.autoSaveInterval = autoSaveInterval;
+    var urlAutoSaveInterval = sync.util.getURLParameter('autoSaveInterval');
+    // The url auto save interval should override the one set in the admin page
+    if(!urlAutoSaveInterval) {
+      var autoSaveInterval = parseInt(sync.options.PluginsOptions.getClientOption('webdav_autosave_interval'));
+      if (e.options.url.match(/^webdav-https?:/)) {
+        e.options.autoSaveInterval = autoSaveInterval;
+      }
     }
   });
 })();
