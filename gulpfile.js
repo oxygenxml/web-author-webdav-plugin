dbe59908d5b6900f3aa5400802f2bec31c1fb4f5
Cristian Banica <cristi_banica@sync.ro>
2020-06-21 23:06:46 +0300

    WA-3980: Upgraded gulp to 4.0.2, changed gulfile.js code

--- gulpfile.js
+++ gulpfile.js
@@ -7,15 +7,20 @@ var Synci18n = require('sync-i18n');
 var fs = require('fs');
 var pluginName = "webdav-integration";
 
-gulp.task('prepare-package', ['i18n'], function() {
+gulp.task('i18n', function (done) {
+  Synci18n().generateTranslations();
+  done();
+});
+
+gulp.task('prepare-package', gulp.series('i18n', function() {
   return gulp.src(['web/*.js'])
     .pipe(concat('plugin.js'))
     .pipe(uglify())
     .pipe(gulp.dest('target/'));
-});
+}));
 
 gulp.task('i18n', function () {
   Synci18n().generateTranslations();
 });
 
-gulp.task('default', ['prepare-package']);
+gulp.task('default', gulp.series('prepare-package'));
